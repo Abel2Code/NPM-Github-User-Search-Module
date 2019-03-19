a0c372e750cc7dd2171c877cf34e56b96c1ecbb8
Abel2Code <asalinaswc1@aol.com>
2019-03-19 16:27:46 -0700

    parses object before returning

--- github-project-search/index.js
+++ github-project-search/index.js
@@ -9,11 +9,21 @@ const _fetch = (command, headers={}) => {
     .catch(error => error.response.error)
 }
 function getProjects(username) {
-  return _fetch(`users/${username}/projects`);
+  return _fetch(`users/${username}/projects`).then((response) => {
+    if(typeof response == 'string') {
+      response = JSON.parse(response);
+    }
+    return response;
+  });
 }
 
 function getProject(projectId, token) {
-  return _fetch(`projects/${projectId}`, headers={'Authorization': `token ${token}`});
+  return _fetch(`projects/${projectId}`, headers={'Authorization': `token ${token}`}).then((response) => {
+    if(typeof response == 'string') {
+      response = JSON.parse(response);
+    }
+    return response;
+  });
 }
 
 module.exports = {
